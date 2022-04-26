462c86235a6c7115a9fa7ed308097534b8a511e9
Bob Kozdemba <bkozdemba@gmail.com>
2022-04-25 20:11:20 -0500

    updated

--- 01-clean-code/pep8_lint_exercise.py
+++ 01-clean-code/pep8_lint_exercise.py
@@ -9,9 +9,17 @@ import time
 import numpy as np
 
 def get_total_price(filepath):
+    """
+    Function to calculate the total price of all books that are
+    less than $25.
 
-    with open(filepath) as f:
-        gift_costs = f.read().split('\n')
+    Args:
+    - filepath (str) The path to the filename of book prices.
+    Returns:
+    - The total price
+    """
+    with open(filepath, encoding="utf-8") as file:
+        gift_costs = file.read().split('\n')
 
     # Remove the trailing null char or the following numpy call will fail.
     # that converts strings to ints.
@@ -21,10 +29,8 @@ def get_total_price(filepath):
     return (gift_costs[gift_costs < 25]).sum() * 1.08
 
 if __name__ == "__main__":
-    """
-    Shell command to generate data a file with 1M integers:
-    $ for i in `seq 2 1000000`; do echo $((1 + $RANDOM % 100)) >> gift_costs.txt ; done
-    """
+    # Shell command to generate data a file with 1M integers:
+    # $ for i in `seq 2 1000000`; do echo $((1 + $RANDOM % 100)) >> gift_costs.txt ; done
     start = time.time()
     total_price = get_total_price('gift_costs.txt')
     print(f'Total ${total_price:0.2f}, Duration: {time.time() - start:0.2f} secs.')
