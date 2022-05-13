9c9b09b2cd07452d5adad16635bcb2b58348d4eb
Bob Kozdemba <bkozdemba@gmail.com>
2022-05-13 13:03:36 -0400

    cleaned up

--- 01-clean-code/project_churn/churn_script_logging_and_tests.py
+++ 01-clean-code/project_churn/churn_script_logging_and_tests.py
@@ -4,11 +4,13 @@ import churn_library as cl
 import pytest
 
 logging.basicConfig(
-    filename='./logs/churn_library.log',
-    level=logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+filename='./logs/churn_library.log',
+level=logging.INFO,
+filemode='w',
+encoding='utf-8',
+format='%(name)s - %(levelname)s - %(message)s')
 
+logger = logging.getLogger()
 
 @pytest.fixture(scope="module")
 def import_data():
@@ -25,16 +27,16 @@ def test_import(import_data):
     '''
     try:
         df = import_data("./data/bank_data.csv")
-        logging.info("Testing import_data: SUCCESS")
+        logger.info("Testing import_data: SUCCESS")
     except FileNotFoundError as err:
-        logging.error("Testing import_eda: The file wasn't found")
+        logger.error("Testing import_eda: The file wasn't found")
         raise err
 
     try:
         assert df.shape[0] > 0
         assert df.shape[1] > 0
     except AssertionError as err:
-        logging.error(
+        logger.error(
             "Testing import_data: The file doesn't appear to have rows and columns")
         raise err
 
