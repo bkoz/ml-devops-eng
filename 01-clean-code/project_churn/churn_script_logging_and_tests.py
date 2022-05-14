53383d3db45dbbc254251ba66774be558386f839
Bob Kozdemba <bkozdemba@gmail.com>
2022-05-14 14:10:15 -0400

    improved testing

--- 01-clean-code/project_churn/churn_script_logging_and_tests.py
+++ 01-clean-code/project_churn/churn_script_logging_and_tests.py
@@ -1,7 +1,17 @@
-import os
+"""
+File: churn_script_logging_and_tests.py
+Description: Python script that define unit test for the churn_library.py functions.
+Author: bkozdemba@gmail.com
+Date: May 2022
+
+Usage:
+$ pytest churn_script_logging_and_tests.py --log-file=logs/churn_library.log --log-level=INFO
+"""
 import logging
-import churn_library as cl
+from genericpath import exists
 import pytest
+import churn_library as cl
+
 
 logging.basicConfig(
 filename='./logs/churn_library.log',
@@ -26,15 +36,15 @@ def test_import(import_data):
     test data import - this example is completed for you to assist with the other test functions
     '''
     try:
-        df = import_data("./data/bank_data.csv")
+        df_local = import_data("./data/bank_data.csv")
         logger.info("Testing import_data: SUCCESS")
     except FileNotFoundError as err:
         logger.error("Testing import_eda: The file wasn't found")
         raise err
 
     try:
-        assert df.shape[0] > 0
-        assert df.shape[1] > 0
+        assert df_local.shape[0] > 0
+        assert df_local.shape[1] > 0
     except AssertionError as err:
         logger.error(
             "Testing import_data: The file doesn't appear to have rows and columns")
@@ -54,14 +64,20 @@ def test_eda(perform_eda):
     '''
     test perform eda function
     '''
-    df = cl.import_data('./data/bank_data.csv')
-    perform_eda(df)
-    assert './images/churn_histogram.png'
-    assert './images/heatmap.png'
-    assert './images/total_transaction_histogram.png'
-    assert './images/customer_age_histogram.png'
-    assert './images/marital_status_counts.png'
-
+    try:
+        df_local = cl.import_data('./data/bank_data.csv')
+        perform_eda(df_local)
+        assert exists('./images/churn_histogram.png')
+        assert exists('./images/heatmap.png')
+        assert exists('./images/total_transaction_histogram.png')
+        assert exists('./images/customer_age_histogram.png')
+        assert exists('./images/marital_status_counts.png')
+        logger.info("Testing perform_eda: SUCCESS")
+        
+    except AssertionError as err:
+        logger.error(
+            "Testing perform_eda: FAILED!")
+        raise err
 
 @pytest.fixture(scope="module")
 def encoder_helper():
@@ -76,8 +92,8 @@ def test_encoder_helper(encoder_helper):
     '''
     test encoder helper
     '''
-    df = cl.import_data('./data/bank_data.csv')
-    cl.perform_eda(df)
+    df_local = cl.import_data('./data/bank_data.csv')
+    cl.perform_eda(df_local)
 
     cat_columns = [
         'Gender',
@@ -104,8 +120,14 @@ def test_encoder_helper(encoder_helper):
         'Avg_Utilization_Ratio'
     ]
 
-    df = encoder_helper(df, cat_columns, None)
-    assert df.shape[0] == 10127
+    try:
+        df_local = encoder_helper(df_local, cat_columns, quant_columns)
+        assert df_local.shape[0] == 10127
+        logger.info("Testing encoder_helper: SUCCESS")
+        
+    except AssertionError as err:
+        logger.error("Testing encoder_helper: FAILED!")
+        raise err
 
 
 @pytest.fixture(scope="module")
@@ -121,8 +143,8 @@ def test_perform_feature_engineering(perform_feature_engineering):
     '''
     test perform_feature_engineering
     '''
-    df = cl.import_data('./data/bank_data.csv')
-    cl.perform_eda(df)
+    df_local = cl.import_data('./data/bank_data.csv')
+    cl.perform_eda(df_local)
 
     cat_columns = [
         'Gender',
@@ -149,10 +171,16 @@ def test_perform_feature_engineering(perform_feature_engineering):
         'Avg_Utilization_Ratio'
     ]
 
-    df = cl.encoder_helper(df, cat_columns, None)
+    df_local = cl.encoder_helper(df_local, cat_columns, None)
 
-    X_train, X_test, y_train, y_test = perform_feature_engineering(df, None)
-    assert X_train.shape[0] == 7088
+    try:
+        x_train, x_test, y_train, y_test = cl.perform_feature_engineering(df_local, None)
+        assert x_train.shape[0] == 7088
+        logger.info("Testing perform_feature_engineering: SUCCESS")
+        
+    except AssertionError as err:
+        logger.error("Testing perform_feature_engineering: FAILED!")
+        raise err
 
 
 @pytest.fixture(scope="module")
@@ -168,7 +196,47 @@ def test_train_models(train_models):
     '''
     test train_models
     '''
+    df_local = cl.import_data('./data/bank_data.csv')
+    cl.perform_eda(df_local)
 
+    cat_columns = [
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category'
+    ]
 
-if __name__ == "__main__":
-    pass
+    quant_columns = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio'
+    ]
+
+    df_local = cl.encoder_helper(df_local, cat_columns, None)
+
+    x_train, x_test, y_train, y_test = cl.perform_feature_engineering(df_local, None)
+
+    try:
+        cl.train_models(x_train, x_test, y_train, y_test)
+        assert exists('models/logistic_model.pkl')
+        assert exists('models/rfc_model.pkl')
+        logger.info("Testing train_models: SUCCESS")
+        
+    except AssertionError as err:
+        logger.error("Testing train_models: FAILED!")
+        raise err
+
+# if __name__ == "__main__":
+#     pass
