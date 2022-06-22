01bdbf7477ad81c1c45fe1b4bb2296d51191d22f
Bob Kozdemba <bkozdemba@gmail.com>
2022-06-22 17:12:29 -0500

    updated

--- 03-scalable-pipes/fastapi/README.md
+++ 03-scalable-pipes/fastapi/README.md
@@ -10,6 +10,16 @@ Python 3.8.13
 pip install uvicorn fastapi
 ```
 
+### Example: A simple GET/POST
+
+File: `tagged.py`
+
+```
+uvicorn tagged:app --reload
+```
+
+Visit `http://127.0.0.1:8000/docs/` and make a `POST` request.
+
 ### Exercise: Parameters and Input in FastAPI
 
 File: `myserver.py`
--- 03-scalable-pipes/fastapi/myserver.py
+++ 03-scalable-pipes/fastapi/myserver.py
@@ -7,15 +7,19 @@ To get started, you can use the following snippet -- note the missing imports:
 """
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Instantiate the app.
 app = FastAPI()
+
+class Value(BaseModel):
+  value: int
 
 # Define a GET on the specified endpoint.
 @app.get("/")
 async def say_hello()-> str:
     return {"greeting": "Hello World!"}
 
-@app.post("/path/{query}")
-async def exercise_function(path: str, query: str, body: str = "")-> dict:
+@app.post("/{path}")
+async def exercise_function(path: int, query: int, body: Value)-> dict:
   return {"path": path, "query": query, "body": body}
