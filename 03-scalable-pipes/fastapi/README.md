699547c432d19f4b91adb98488dd279097bfe8fe
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-08 15:35:11 -0500

    updated

--- 03-scalable-pipes/fastapi/README.md
+++ 03-scalable-pipes/fastapi/README.md
@@ -20,6 +20,18 @@ uvicorn tagged:app --reload
 
 Visit `http://127.0.0.1:8000/docs/` and make a `POST` request.
 
+```
+curl -X 'POST' 'http://localhost:8000/predict/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{"sepal_length": 5.4, "sepal_width": 3, "pedal_length": 3.75, "pedal_width": 1.1}'
+
+{"Prediction":1}
+```
+
+### Logging
+```
+logger = logging.getLogger("uvicorn.info")
+logger.info("model_server: app is up!")
+```
+
 ### Exercise: Parameters and Input in FastAPI
 
 File: `myserver.py`
--- 03-scalable-pipes/fastapi/myserver.py
+++ 03-scalable-pipes/fastapi/myserver.py
@@ -8,22 +8,54 @@ To get started, you can use the following snippet -- note the missing imports:
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import numpy as np
+import json
+import pickle
 import logging
 
 # Instantiate the app.
 app = FastAPI()
-logger = logging.getLogger("uvicorn.info")
 
-logger.info("HELLO")
+logger = logging.getLogger("uvicorn.info")
+logger.info("model_server: app is up!")
+
+logger.info("model_server: Loading model.")
+try:
+    model = pickle.load(open("iris_rfc.pkl", "rb"))
+    logger.info(f'model_server: Iris model loaded.')
+except Exception as e:
+    logger.info(f'model_server: Load model failed!!!')
+    model = None
 
 class Value(BaseModel):
   value: int
 
-# Define a GET on the specified endpoint.
+class Iris(BaseModel):
+    sepal_length: float = 5.4
+    sepal_width: float = 3
+    pedal_length: float = 3.75
+    pedal_width: float = 1.1
+    
+
 @app.get("/")
 async def say_hello()-> str:
+    logger.info("model_server: say_hello() GET")
     return {"greeting": "Hello World!"}
+
 
 @app.post("/{path}")
 async def exercise_function(path: int, query: int, body: Value)-> dict:
+  logger.info(f"model_server: exercise_function() POST: path={path} query={query} body={body}")
   return {"path": path, "query": query, "body": body}
+
+
+@app.post("/predict/")
+async def make_prediction(body: Iris)-> dict:
+  global model
+  logger.info(f"model_server: make_prediction() POST: body={body}")
+  predict_request = np.array([body.sepal_length, body.sepal_width, body.pedal_length, body.pedal_width]).reshape(1,-1)
+  ret = model.predict(predict_request)
+  # Convert to an int.
+  serialized = ret.tolist()[0]
+  logger.info(f"model_server: prediction = {serialized}")
+  return {"Prediction": serialized}
